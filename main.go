9b5b6a3d8f41215ddced082b282e467af41d7b59
Jordan Orelli <jordanorelli@gmail.com>
2021-12-06 20:09:46 -0600

    cleaning some stuff up

--- main.go
+++ main.go
@@ -8,9 +8,6 @@ import (
 	"log"
 	"os"
 	"os/signal"
-
-	"orel.li/modularium/internal/index"
-	"orel.li/modularium/internal/ref"
 )
 
 var log_error = log.New(os.Stderr, "", 0)
@@ -39,30 +36,6 @@ func sigCancel(ctx context.Context) context.Context {
 		shutdown(nil)
 	}()
 	return ctx
-}
-
-func serve(args []string) {
-	path := "./modularium.sock"
-	indexPath := pathArg{path: "./modules-index.json"}
-
-	serveFlags := flag.NewFlagSet("serve", flag.ExitOnError)
-	serveFlags.StringVar(&path, "l", path, "path for a unix domain socket to listen on")
-	serveFlags.Var(&indexPath, "index", "an index config")
-	serveFlags.Parse(args)
-
-	idx, err := index.Load(indexPath.path)
-	if err != nil {
-		shutdown(err)
-	}
-	log_info.Printf("index: %v", idx)
-
-	h := handler{
-		path:  ref.New(&path),
-		index: ref.New(&indexPath),
-	}
-	if err := h.run(); err != nil {
-		bail(1, err.Error())
-	}
 }
 
 func main() {
---
+++ serve.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"flag"
+
+	"orel.li/modularium/internal/index"
+	"orel.li/modularium/internal/ref"
+)
+
+func serve(args []string) {
+	path := "./modularium.sock"
+	indexPath := pathArg{path: "./modules-index.json"}
+
+	serveFlags := flag.NewFlagSet("serve", flag.ExitOnError)
+	serveFlags.StringVar(&path, "l", path, "path for a unix domain socket to listen on")
+	serveFlags.Var(&indexPath, "index", "an index config")
+	serveFlags.Parse(args)
+
+	idx, err := index.Load(indexPath.path)
+	if err != nil {
+		shutdown(err)
+	}
+	log_info.Printf("index: %v", idx)
+
+	h := handler{
+		path:  ref.New(&path),
+		index: ref.New(&indexPath),
+	}
+	if err := h.run(); err != nil {
+		bail(1, err.Error())
+	}
+}
